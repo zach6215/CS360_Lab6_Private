f22cec969ce3c4be86e2382b8196bd84bbdd7b1b
Zachary Hall <zachary.c.hall@wsu.edu>
2022-03-20 18:49:53 -0700

    Intial Commit

---
+++ main.c
@@ -0,0 +1,213 @@
+/****************************************************************************
+*                   KCW: mount root file system                             *
+*****************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <ext2fs/ext2_fs.h>
+#include <string.h>
+#include <libgen.h>
+#include <sys/stat.h>
+#include <time.h>
+
+#include "type.h"
+
+extern MINODE *iget();
+
+MINODE minode[NMINODE];
+MINODE *root;
+PROC   proc[NPROC], *running;
+
+char gpath[128]; // global for tokenized components
+char *name[64];  // assume at most 64 components in pathname
+int   n;         // number of component strings
+
+int fd, dev;
+int nblocks, ninodes, bmap, imap, iblk;
+char line[128], cmd[32], pathname[128];
+
+#include "cd_ls_pwd.c"
+
+int get_block(int dev, int blk, char *buf)
+{
+   lseek(dev, (long)blk*BLKSIZE, 0);
+   read(dev, buf, BLKSIZE);
+}
+
+void iput(MINODE *mip)
+{
+ int i, block, offset;
+ char buf[BLKSIZE];
+ INODE *ip;
+
+ if (mip==0) 
+     return;
+
+ mip->refCount--;
+ 
+ if (mip->refCount > 0) return;
+ if (!mip->dirty)       return;
+ 
+ /* write INODE back to disk */
+ /**************** NOTE ******************************
+  For mountroot, we never MODIFY any loaded INODE
+                 so no need to write it back
+  FOR LATER WROK: MUST write INODE back to disk if refCount==0 && DIRTY
+
+  Write YOUR code here to write INODE back to disk
+ *****************************************************/
+} 
+
+// return minode pointer to loaded INODE
+MINODE *iget(int dev, int ino)
+{
+  int i;
+  MINODE *mip;
+  char buf[BLKSIZE];
+  int blk, offset;
+  INODE *ip;
+
+  for (i=0; i<NMINODE; i++){
+    mip = &minode[i];
+    if (mip->refCount && mip->dev == dev && mip->ino == ino){
+       mip->refCount++;
+       //printf("found [%d %d] as minode[%d] in core\n", dev, ino, i);
+       return mip;
+    }
+  }
+    
+  for (i=0; i<NMINODE; i++){
+    mip = &minode[i];
+    if (mip->refCount == 0){
+       //printf("allocating NEW minode[%d] for [%d %d]\n", i, dev, ino);
+       mip->refCount = 1;
+       mip->dev = dev;
+       mip->ino = ino;
+
+       // get INODE of ino to buf    
+       blk    = (ino-1)/8 + iblk;
+       offset = (ino-1) % 8;
+
+       //printf("iget: ino=%d blk=%d offset=%d\n", ino, blk, offset);
+
+       get_block(dev, blk, buf);
+       ip = (INODE *)buf + offset;
+       // copy INODE to mp->INODE
+       mip->INODE = *ip;
+       return mip;
+    }
+  }   
+  printf("PANIC: no more free minodes\n");
+  return 0;
+}
+
+int init()
+{
+  int i, j;
+  MINODE *mip;
+  PROC   *p;
+
+  printf("init()\n");
+
+  for (i=0; i<NMINODE; i++){
+    mip = &minode[i];
+    mip->dev = mip->ino = 0;
+    mip->refCount = 0;
+    mip->mounted = 0;
+    mip->mptr = 0;
+  }
+  for (i=0; i<NPROC; i++){
+    p = &proc[i];
+    p->pid = i;
+    p->uid = p->gid = 0;
+    p->cwd = 0;
+  }
+}
+
+// load root INODE and set root pointer to it
+int mount_root()
+{  
+  printf("mount_root()\n");
+  root = iget(dev, 2);
+}
+
+char *disk = "diskimage";
+int main(int argc, char *argv[ ])
+{
+  int ino;
+  char buf[BLKSIZE];
+
+  printf("checking EXT2 FS ....");
+  if ((fd = open(disk, O_RDWR)) < 0){
+    printf("open %s failed\n", disk);
+    exit(1);
+  }
+
+  dev = fd;    // global dev same as this fd   
+
+  /********** read super block  ****************/
+  get_block(dev, 1, buf);
+  sp = (SUPER *)buf;
+
+  /* verify it's an ext2 file system ***********/
+  if (sp->s_magic != 0xEF53){
+      printf("magic = %x is not an ext2 filesystem\n", sp->s_magic);
+      exit(1);
+  }     
+  printf("EXT2 FS OK\n");
+  ninodes = sp->s_inodes_count;
+  nblocks = sp->s_blocks_count;
+
+  get_block(dev, 2, buf); 
+  gp = (GD *)buf;
+
+  bmap = gp->bg_block_bitmap;
+  imap = gp->bg_inode_bitmap;
+  iblk = gp->bg_inode_table;
+  printf("bmp=%d imap=%d inode_start = %d\n", bmap, imap, iblk);
+
+  init();  
+  mount_root();
+  printf("root refCount = %d\n", root->refCount);
+
+  printf("creating P0 as running process\n");
+  running = &proc[0];
+  running->cwd = iget(dev, 2);
+  printf("root refCount = %d\n", root->refCount);
+
+  // WRTIE code here to create P1 as a USER process
+  
+  while(1){
+    printf("input command : [ls|cd|pwd|quit] ");
+    fgets(line, 128, stdin);
+    line[strlen(line)-1] = 0;
+
+    if (line[0]==0)
+       continue;
+    pathname[0] = 0;
+
+    sscanf(line, "%s %s", cmd, pathname);
+    printf("cmd=%s pathname=%s\n", cmd, pathname);
+  
+    if (strcmp(cmd, "ls")==0)
+       ls();
+    else if (strcmp(cmd, "cd")==0)
+       cd();
+    else if (strcmp(cmd, "pwd")==0)
+       pwd(running->cwd);
+    else if (strcmp(cmd, "quit")==0)
+       quit();
+  }
+}
+
+int quit()
+{
+  int i;
+  MINODE *mip;
+  for (i=0; i<NMINODE; i++){
+    mip = &minode[i];
+    if (mip->refCount > 0)
+      iput(mip);
+  }
+  exit(0);
+}
